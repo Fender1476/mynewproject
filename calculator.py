0dfe48b5edbaa9a940de2b3a10c24a14bd542095
Дмитрий <tchuryuomv12@gmail.com>
2022-11-06 19:18:15 +0300

    new version app calculator.py

--- calculator.py
+++ calculator.py
@@ -1,1 +1,22 @@
-print(1+1)
+'''
+Калькулятор который будет выполнять стандартные вычисления
+'''
+
+class Calculator: 
+
+    def __init__(self, one_digit, two_digit, action):
+        self.one_digit = one_digit
+        self.two_digit = two_digit
+        self.action = action
+
+    def sum(self):
+        if self.action == '+':
+            return self.one_digit + self.two_digit
+
+
+    def __str__(self) :
+        return int(self.one_digit) + int(self.two_digit)
+
+
+one_action = Calculator(1,2,'+')
+print(one_action)
